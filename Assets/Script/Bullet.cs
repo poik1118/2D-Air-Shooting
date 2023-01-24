2d85b7a5a2565d2c39022fa8ddb4231cd70311da
Edoc <79559496+poik1118@users.noreply.github.com>
2023-01-25 02:57:24 +0900

    Update Bullet.cs

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float Damege;
+    public float            Damege;
 
     
     private void OnTriggerEnter2D(Collider2D collision)
